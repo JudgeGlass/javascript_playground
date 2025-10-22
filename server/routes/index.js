6df099666698aa85632b299f9a315b4b4c68c1ca
Hunter Wilcox <hunter.wilcox@judgeglass.net>
2025-10-21 21:49:47 -0500

    user add / user update

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const path = require('path');
-const get_database = require('../scripts/database.js');
+const { get_database, add_user, update_user } = require('../scripts/database.js');
 var router = express.Router();
 
 async function read_db()
@@ -21,6 +21,24 @@ router.get('/test2', function(req, res, next) {
   console.log(`Var1: ${req.query.var1} - Var2: ${req.query.var2}`)
   read_db()
   res.sendFile(path.join(__dirname + '/../pages/index.html'))
+});
+
+router.post('/useradd', function(req, res, next) {
+  user_info = req.body
+  add_user(user_info).then(rc => {
+    res.status(rc)
+    console.log("RC: " + rc)
+  })
+  res.json({requestBody: "OK"})
+});
+
+router.put('/userupdate', function(req, res, next) {
+  user_info = req.body
+  update_user(user_info).then(rc => {
+    res.status(rc)
+    console.log("RC: " + rc)
+  })
+  res.json({requestBody: "OK"})
 });
 
 module.exports = router;
--- server/scripts/database.js
+++ server/scripts/database.js
@@ -23,4 +23,49 @@ async function get_database() {
   }
 }
 
-module.exports = get_database
+async function employee_exists(db_id, database)
+{
+  const result = await database.collection('Employees').find({"db_id": db_id}).toArray();
+  console.log("LEN: " + result.length)
+  return result.length > 0
+}
+
+async function add_user(user_data)
+{
+  rc = 200
+  database = await get_database()
+  
+  db_id = user_data['db_id']
+  if (await employee_exists(db_id, database))
+  {
+    return 400
+  }
+
+  const result = await database.collection('Employees')
+  result.insertOne(user_data, function(err, res){
+    if (err) throw err;
+    console.log("1 user inserted");
+    database.close();
+  });
+
+  return rc
+}
+
+async function update_user(user_data)
+{
+  rc = 200
+  database = await get_database()
+  
+  db_id = user_data['db_id']
+  if (!await employee_exists(db_id, database))
+  {
+    return 500
+  }
+
+  const collection = await database.collection('Employees')
+  const result = await collection.updateOne({ "db_id": db_id }, { $set: user_data }, {})
+
+  return rc
+}
+
+module.exports = { get_database, add_user, update_user }
